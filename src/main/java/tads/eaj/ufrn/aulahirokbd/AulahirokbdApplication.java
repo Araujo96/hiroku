6eb5da9e400872d94a2887df26f6ef7bdca85844
Araujo96 <lucasccp1996@hotmail.com>
2021-07-01 15:42:50 -0300

    Initial commit

---
+++ src/main/java/tads/eaj/ufrn/aulahirokbd/AulahirokbdApplication.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.aulahirokbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulahirokbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AulahirokbdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tads/eaj/ufrn/aulahirokbd/AulahirokbdApplicationTests.java
@@ -0,0 +1,13 @@
+package tads.eaj.ufrn.aulahirokbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulahirokbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
